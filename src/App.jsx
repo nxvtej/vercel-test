c6fa44b0f3a4bce9fc0cd8253790ed063fb98982
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-11-25 15:04:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,14 @@ function App() {
 			<h1>Vite + React</h1>
 			<div className='card'>
 				<button onClick={() => setCount((count) => count + 1)}>
-					Testing with count whose value is:{count}
+					 Count is:{count}
 				</button>
 				<p>
 					Edit <code>src/App.jsx</code> and save to test HMR
 				</p>
 			</div>
 			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
+				Testing navi's hosting service with react code
 			</p>
 		</>
 	);
